a2665a4aa323a6aa60d55b97a88e8edc9af96b86
Matt Kramer <matt314159@gmail.com>
2023-02-03 14:02:02 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ module load python
 module load cray-hdf5-parallel
 conda create -n module0_flow_env --clone lazy-h5py
 conda activate module0_flow_env
+pip install --upgrade pip
 
 # See setup.py from module0_flow and h5flow for dependencies.
 # Some are already installed; others aren't strictly necessary.
@@ -13,10 +14,18 @@ conda activate module0_flow_env
 
 pip install scipy scikit-image scikit-learn pyyaml pyyaml-include tqdm
 
-pip install git+https://github.com/peter-madigan/h5flow.git#egg=h5flow
+pip install git+https://github.com/peter-madigan/h5flow.git
 
-git clone https://github.com/peter-madigan/module0_flow.git -b module2/add-module2-yamls
+# git clone https://github.com/peter-madigan/module0_flow.git -b module2/add-module2-yamls
 # ( cd module0_flow && python setup.py install )
-export PYTHONPATH=$PWD/module0_flow:$PYTHONPATH
+# export PYTHONPATH=$PWD/module0_flow:$PYTHONPATH
 
-pip install git+https://github.com/mjkramer/zeroworker.git#egg=zeroworker
+# pip install git+https://github.com/mjkramer/module0_flow.git@module3-nersc
+
+git clone https://github.com/mjkramer/module0_flow.git -b module3-nersc
+( cd module0_flow && pip install . )
+
+pip install git+https://github.com/mjkramer/zeroworker.git
+
+# for accessing the run log (drift field, light file):
+pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib
